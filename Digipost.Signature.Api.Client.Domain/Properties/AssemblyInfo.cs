327a11a028105d4db308233ac597dbf60217ef8e
Aleksander Aas Sjåfjell <aleksander.sjafjell@bekk.no>
2015-12-08 12:33:59 +0100

    Adds SharedAssemblyVersion
    All projects in solution will now have same version number. Always.

--- Digipost.Signature.Api.Client.Domain/Properties/AssemblyInfo.cs
+++ Digipost.Signature.Api.Client.Domain/Properties/AssemblyInfo.cs
@@ -22,18 +22,5 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("2d13f732-6a85-4165-ae2f-a87c09750398")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
-
 [assembly: InternalsVisibleTo("Digipost.Signature.Api.Client.Tests,PublicKey=00240000048000009400000006020000002400005253413100040000010001008b3388f9c416425f0145bbcf26e66b9a87c4e08b4cd41563e4bc8846df38ba4d997c5408cc77da26d79b03c39874a6af9df0aff3e7bdb3c0e53a91f6d19c50e160f5bf67986a04f0f985eca0252f557ed9ae520dd51e3107d6168d073d4ec5ada28d34e492ad9fb7af29c82309c5c0124211e679caea38d5463d2af9042dafda")]
 

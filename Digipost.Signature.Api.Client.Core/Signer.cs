a361508a057cb4d1c5eb5578767f42d640b3a4bb
Aleksander Aas Sjåfjell <aleksander.sjafjell@bekk.no>
2016-01-07 10:21:28 +0100

    Adds solution files for serialization

--- Digipost.Signature.Api.Client.Core/Signer.cs
+++ Digipost.Signature.Api.Client.Core/Signer.cs
@@ -1,5 +1,11 @@
-﻿namespace Digipost.Signature.Api.Client.Core
+﻿using System;
+using System.Xml.Serialization;
+
+namespace Digipost.Signature.Api.Client.Core
 {
+    [Serializable]
+    [XmlRoot(Namespace = "http://signering.digipost.no/schema/v1", IsNullable = false)]
+    [XmlType(TypeName = "signer", Namespace = "http://signering.digipost.no/schema/v1/common")]
     public class Signer
     {
         public string PersonalIdentificationNumber { get;}

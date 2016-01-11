4068c5488418e05d859fedb5cf935a05077c3ac3
Aleksander Aas Sjåfjell <aleksander.sjafjell@bekk.no>
2016-01-11 10:54:02 +0100

    Adds responseUrls
    Wraps class of urls returned in DirectJobResponse

---
+++ Digipost.Signature.Api.Client.Direct/ResponseUrls.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Digipost.Signature.Api.Client.Direct
+{
+    public class ResponseUrls
+    {
+        public Uri Redirect { get; set; }
+        public Uri Status { get; set; }
+
+        public ResponseUrls(Uri redirectUrl, Uri statusUrl)
+        {
+            Redirect = redirectUrl;
+            Status = statusUrl;
+        }
+    }
+}

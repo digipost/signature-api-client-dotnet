c0df9b15c91fd8e26b0ab2035281c1826fb838d0
Aleksander Aas Sjåfjell <aleksander.sjafjell@bekk.no>
2016-01-11 10:21:51 +0100

    Adds ClientConfiguration
    ClientConfiguration and tests

--- Digipost.Signature.Api.Client.Core/ClientConfiguration.cs
+++ Digipost.Signature.Api.Client.Core/ClientConfiguration.cs
@@ -1,7 +1,21 @@
-﻿namespace Digipost.Signature.Api.Client.Core
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Digipost.Signature.Api.Client.Core
 {
     public class ClientConfiguration
     {
+        public Uri SignatureServiceRoot { get; internal set; }
 
+        public Sender Sender { get; internal set; }
+
+        public X509Certificate2 Certificate { get; internal set; }
+
+        public ClientConfiguration(Uri signatureServiceRoot, Sender sender, X509Certificate2 certificate)
+        {
+            SignatureServiceRoot = signatureServiceRoot;
+            Sender = sender;
+            Certificate = certificate;
+        }
     }
 }

01432ca7cd0a36dca87c2e4620f86668c8a125ea
Aleksander Aas Sjåfjell <aleksander.sjafjell@bekk.no>
2016-01-22 11:08:39 +0100

    Adds fields to DirectJobReference and tests

---
+++ Digipost.Signature.Api.Client.Direct.Tests/DirectJobReferenceTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Digipost.Signature.Api.Client.Direct.Tests
+{
+    [TestClass]
+    public class DirectJobReferenceTests
+    {
+        [TestClass]
+        public class ConstructorMethod : DirectJobReferenceTests
+        {
+            [TestMethod]
+            public void SimpleConstructor()
+            {
+                //Arrange
+                var jobReference = new Uri("http://signatureserviceroot.digipost.no/urlidurl/id030302");
+
+                //Act
+                var directJobReference = new DirectJobReference(jobReference);
+
+                //Assert
+                Assert.AreEqual(jobReference,directJobReference.Reference);
+            }
+        }
+    }
+
+}

eb1ea068447111dae938d058fda9f81f7517658a
Aleksander Aas Sjåfjell <aleksander.sjafjell@bekk.no>
2016-01-25 15:56:36 +0100

    Adds methods for Xades and Pades
    No tests are added except smoke-tests. These are ignore since TeamCity has no Signature backend.

--- Digipost.Signature.Api.Client.Direct.Tests/Smoke/DirectClientSmokeTests.cs
+++ Digipost.Signature.Api.Client.Direct.Tests/Smoke/DirectClientSmokeTests.cs
@@ -1,7 +1,8 @@
 ﻿using System;
-using System.Diagnostics;
+using System.IO;
 using System.Threading.Tasks;
 using Digipost.Signature.Api.Client.Core;
+using Digipost.Signature.Api.Client.Core.Tests.Asice.AsiceManifest;
 using Digipost.Signature.Api.Client.Core.Tests.Utilities;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -10,18 +11,7 @@ namespace Digipost.Signature.Api.Client.Direct.Tests.Smoke
     [TestClass]
     public class DirectClientSmokeTests
     {
-        private static DirectClient DirectClient(Sender sender)
-        {
-            var directClient = new DirectClient(
-                new ClientConfiguration(
-                    new Uri("https://172.16.91.1:8443"),
-                    sender,
-                    DomainUtility.GetTestIntegrasjonSertifikat()
-                    )
-                );
-            return directClient;
-        }
-
+        [Ignore]
         [TestClass]
         public class CreateMethod : DirectClientSmokeTests
         {
@@ -41,6 +31,7 @@ namespace Digipost.Signature.Api.Client.Direct.Tests.Smoke
             }
         }
 
+        [Ignore]
         [TestClass]
         public class GetStatusMethod : DirectClientSmokeTests
         {
@@ -52,15 +43,84 @@ namespace Digipost.Signature.Api.Client.Direct.Tests.Smoke
                 var directClient = DirectClient(sender);
                 var directJob = new DirectJob(sender, DomainUtility.GetDocument(), DomainUtility.GetSigner(), "SmokeTestReference", DomainUtility.GetExitUrls());
                 var jobResponse = await directClient.Create(directJob);
-
-                Trace.WriteLine(jobResponse.ResponseUrls.Redirect);
-
+                
                 //Act
                 var jobStatus = await directClient.GetStatus(jobResponse.DirectJobReference);
 
                 //Assert
                 Assert.IsNotNull(jobStatus.JobId);
             }
+        }
+
+        [Ignore]
+        [TestClass]
+        public class GetXadesMethod : DirectClientSmokeTests
+        {
+            [TestMethod]
+            public async Task GetsXadesSuccessfully()
+            {
+                //Arrange
+                var sender = DomainUtility.GetSender();
+                var directJob = DomainUtility.GetDirectJob();
+                var directClient = DirectClient(sender);
+                //var jobResponse = await directClient.Create(directJob);
+                var directJobReference = new DirectJobReference(new Uri("https://172.16.91.1:8443/api/signature-jobs/121/status"));
+                var jobStatus = await directClient.GetStatus(directJobReference);
+
+                //Act
+                var xadesReference = new XadesReference(jobStatus.StatusResponseUrls.Xades);
+                using (Stream xadesStream = await directClient.GetXades(xadesReference))
+                {
+                    using (MemoryStream ms = new MemoryStream())
+                    {
+                        xadesStream.CopyTo(ms);
+                        File.WriteAllBytes(@"C:\Users\aas\Downloads\xades.xml", ms.ToArray());
+                    }
+                };
+
+                //Assert
+            }
+        }
+
+        [Ignore]
+        [TestClass]
+        public class GetPadesMethod : DirectClientSmokeTests
+        {
+            [TestMethod]
+            public async Task GetsPadesSuccessfully()
+            {
+                //Arrange
+                var sender = DomainUtility.GetSender();
+                var directJob = DomainUtility.GetDirectJob();
+                var directClient = DirectClient(sender);
+                //var jobResponse = await directClient.Create(directJob);
+                var directJobReference = new DirectJobReference(new Uri("https://172.16.91.1:8443/api/signature-jobs/121/status"));
+                var jobStatus = await directClient.GetStatus(directJobReference);
+
+                //Act
+                var padesReference = new PadesReference(jobStatus.StatusResponseUrls.Pades);
+                using (Stream xadesStream = await directClient.GetPades(padesReference))
+                {
+                    using (MemoryStream ms = new MemoryStream())
+                    {
+                        xadesStream.CopyTo(ms);
+                        File.WriteAllBytes(@"C:\Users\aas\Downloads\pades2.pdf", ms.ToArray());
+                    }
+                };
+            }
+        }
+
+
+        private static DirectClient DirectClient(Sender sender)
+        {
+            var directClient = new DirectClient(
+                new ClientConfiguration(
+                    new Uri("https://172.16.91.1:8443"),
+                    sender,
+                    DomainUtility.GetTestIntegrasjonSertifikat()
+                    )
+                );
+            return directClient;
         }
     }
 }

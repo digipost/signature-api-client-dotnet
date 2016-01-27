cdaef5cb029ad6715c87a5f739b4846b87c36f66
Aleksander Aas Sjåfjell <aleksander.sjafjell@bekk.no>
2016-01-27 12:38:06 +0100

    Adds loading of certificate via thumbprint in ClientConfiguration
    - In addition: Adds ignore of Compiler warning 1591 - no xml documentation of public field or property. We do not need to dummy comment all fields. Disables CS7035 - Build format not conforming to standard. It actually does, so this compiler warning is useless.

--- Digipost.Signature.Api.Client.Core.Tests/ClientConfigurationTests.cs
+++ Digipost.Signature.Api.Client.Core.Tests/ClientConfigurationTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
 using Digipost.Signature.Api.Client.Core.Tests.Utilities;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -12,7 +13,7 @@ namespace Digipost.Signature.Api.Client.Core.Tests
         public class ConstructorMethod : ClientConfigurationTests
         {
             [TestMethod]
-            public void SimpleConstructor()
+            public void ConstructorWithCertificate()
             {
                 //Arrange
                 var signatureServiceRoot = new Uri("http://SignatureServiceRoot.no");
@@ -21,18 +22,50 @@ namespace Digipost.Signature.Api.Client.Core.Tests
 
                 //Act
                 var clientConfiguration = new ClientConfiguration(
-                    signatureServiceRoot, 
-                    sender, 
+                    signatureServiceRoot,
+                    sender,
                     x509Certificate
                     );
-                
+
                 //Assert
                 Assert.AreEqual(signatureServiceRoot, clientConfiguration.SignatureServiceRoot);
                 Assert.AreEqual(sender, clientConfiguration.Sender);
                 Assert.AreEqual(x509Certificate, clientConfiguration.Certificate);
             }
-        }
 
+            [TestMethod]
+            public void ConstructorWithCertificateThumbprint()
+            {
+                //Arrange
+                var signatureServiceRoot = new Uri("http://SignatureServiceRoot.no");
+                var sender = DomainUtility.GetSender();
+
+                var aCertificateFromCertificateStore = GetFirstCertificateFromCurrentUserStore();
+
+                //Act
+                var clientConfiguration = new ClientConfiguration(
+                    signatureServiceRoot,
+                    sender,
+                    aCertificateFromCertificateStore.Thumbprint
+                    );
+
+                //Assert
+                Assert.AreEqual(signatureServiceRoot, clientConfiguration.SignatureServiceRoot);
+                Assert.AreEqual(sender, clientConfiguration.Sender);
+                Assert.AreEqual(aCertificateFromCertificateStore, clientConfiguration.Certificate);
+            }
+
+            private static X509Certificate2 GetFirstCertificateFromCurrentUserStore()
+            {
+                var store = new X509Store(StoreName.My,StoreLocation.CurrentUser);
+
+                store.Open(OpenFlags.ReadOnly);
+                var certificate = store.Certificates[0];
+                store.Close();
+
+                return certificate;
+            }
+        }
 
     }
 }

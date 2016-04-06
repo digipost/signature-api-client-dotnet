31d4d4d6b43af45a2ab4d0a0e2783fc3106293a4
Aleksander Aas Sjåfjell <aleksander.sjafjell@bekk.no>
2016-04-06 13:16:58 +0200

    Adds XmlDoc to ClientConfiguration

--- Digipost.Signature.Api.Client.Core/ClientConfiguration.cs
+++ Digipost.Signature.Api.Client.Core/ClientConfiguration.cs
@@ -8,11 +8,28 @@ namespace Digipost.Signature.Api.Client.Core
 {
     public class ClientConfiguration : IAsiceConfiguration
     {
+        /// <param name="environment">The environment which all requests with this instance of the configuration connects to.</param>
+        /// <param name="certificateThumbprint">
+        ///     The thumbprint of the <see cref="Sender">Senders</see> certificate.  All certificates
+        ///     have a thumbprint which uniquely identifies them. If installed in certificate store of current user, the thumbprint
+        ///     can
+        ///     be used to retreieve it. Remember to add it to the store as  exportable to use it in Signature client.
+        /// </param>
+        /// <param name="globalSender">
+        ///     If set, it will be used for all <see cref="ISignatureJob">SignatureJobs</see> created without
+        ///     a <see cref="Sender" />.
+        /// </param>
         public ClientConfiguration(Environment environment, string certificateThumbprint, Sender globalSender = null)
             : this(environment, CertificateUtility.SenderCertificate(certificateThumbprint, Language.English), globalSender)
         {
         }
 
+        /// <param name="environment">The environment which all requests with this instance of the configuration connects to.</param>
+        /// <param name="certificate">Certificate of the <see cref="Sender" />.</param>
+        /// <param name="globalSender">
+        ///     If set, it will be used for all <see cref="ISignatureJob">SignatureJobs</see> created without a
+        ///     <see cref="Sender" />.
+        /// </param>
         public ClientConfiguration(Environment environment, X509Certificate2 certificate, Sender globalSender = null)
         {
             Environment = environment;
@@ -22,6 +39,10 @@ namespace Digipost.Signature.Api.Client.Core
 
         public Environment Environment { get; }
 
+        /// <summary>
+        ///     If set, it will be used for all <see cref="ISignatureJob">SignatureJobs</see> created without
+        ///     a <see cref="Sender" />.
+        /// </summary>
         public Sender GlobalSender { get; internal set; }
 
         public X509Certificate2 Certificate { get; internal set; }

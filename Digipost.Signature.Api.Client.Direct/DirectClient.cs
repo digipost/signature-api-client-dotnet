8b6b97ecc9fc65f1cd7cbe4c3c6f830a9a89f034
Aleksander Aas Sjåfjell <aleksander.sjafjell@bekk.no>
2016-04-06 10:57:04 +0200

    Adds XmlDoc to DirectClient

--- Digipost.Signature.Api.Client.Direct/DirectClient.cs
+++ Digipost.Signature.Api.Client.Direct/DirectClient.cs
@@ -8,6 +8,7 @@ using System.Threading.Tasks;
 using Digipost.Signature.Api.Client.Core;
 using Digipost.Signature.Api.Client.Core.Asice;
 using Digipost.Signature.Api.Client.Direct.DataTransferObjects;
+using Digipost.Signature.Api.Client.Direct.Enums;
 using Digipost.Signature.Api.Client.Direct.Internal;
 using Digipost.Signature.Api.Client.Direct.Internal.AsicE;
 using log4net;
@@ -42,6 +43,18 @@ namespace Digipost.Signature.Api.Client.Direct
             return new Uri($"/api/{directJob.Sender.OrganizationNumber}/direct/signature-jobs", UriKind.Relative);
         }
 
+        /// <summary>
+        ///     Get the current status for the given <see cref="StatusReference" />, which references the status for a specific
+        ///     job.
+        ///     When processing of the status is complete (e.g. retrieving <see cref="GetPades(PadesReference)" /> and/or
+        ///     <see cref="GetXades(XadesReference)" /> documents for a <see cref="JobStatus.Signed" /> job), the returned  status
+        ///     must be confirmed via <see cref="Confirm(ConfirmationReference)" />.
+        /// </summary>
+        /// <param name="statusReference">The reference to the status of a specific job.</param>
+        /// <returns>
+        ///     the <see cref="DirectJobStatusResponse" /> for the job referenced by the given <see cref="StatusReference" />,
+        ///     never null.
+        /// </returns>
         public async Task<DirectJobStatusResponse> GetStatus(StatusReference statusReference)
         {
             var request = new HttpRequestMessage
@@ -75,6 +88,14 @@ namespace Digipost.Signature.Api.Client.Direct
             return await RequestHelper.GetStream(padesReference.Url);
         }
 
+        /// <summary>
+        ///     Confirms that the status retrieved from <see cref="GetStatus(StatusReference)" /> is received. If the confirmed
+        ///     <see cref="JobStatus" /> is a terminal status (e.g. <see cref="JobStatus.Signed" /> or
+        ///     <see cref="JobStatus.Rejected" />),
+        ///     the Signature service may make the job's associated resources unavailable through the API when receiving the
+        ///     confirmation. Calling this method for a response with no <see cref="ConfirmationReference" /> has no effect.
+        /// </summary>
+        /// <param name="confirmationReference">the updated status retrieved from <see cref="GetStatus(StatusReference)" /></param>
         public async Task Confirm(ConfirmationReference confirmationReference)
         {
             await RequestHelper.Confirm(confirmationReference);

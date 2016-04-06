21b45a8b89b009d403bc0ba889529568e51214c5
Aleksander Aas Sjåfjell <aleksander.sjafjell@bekk.no>
2016-04-06 14:05:49 +0200

    Adds XmlDoc to JobStatus

--- Digipost.Signature.Api.Client.Direct/Enums/JobStatus.cs
+++ Digipost.Signature.Api.Client.Direct/Enums/JobStatus.cs
@@ -2,8 +2,19 @@
 {
     public enum JobStatus
     {
+        /// <summary>
+        /// The document(s) of the job has been signed by the receiver.
+        /// </summary>
         Failed,
+
+        /// <summary>
+        /// The signature job has been rejected by the receiver.
+        /// </summary>
         Rejected,
+
+        /// <summary>
+        /// An error occured during the signing ceremony.
+        /// </summary>
         Signed
     }
 }
--- Digipost.Signature.Api.Client.Portal/Enums/JobStatus.cs
+++ Digipost.Signature.Api.Client.Portal/Enums/JobStatus.cs
@@ -2,8 +2,19 @@
 {
     public enum JobStatus
     {
+        /// <summary>
+        /// There has not been any changes since the last received status change.
+        /// </summary>
         NoChanges,
+
+        /// <summary>
+        /// When the client confirms a job with this status, the job is removed from the queue and will not be returned upon subsequent polling until the status has changed again.
+        /// </summary>
         PartiallyCompleted,
+
+        /// <summary>
+        /// When the client confirms a job with this status, the job and its associated resources will become unavailable through the Signature API.
+        /// </summary>
         Completed
     }
 }

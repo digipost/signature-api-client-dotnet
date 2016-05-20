bc8fe19e46b3bfd88afab701093a67414a6d614d
Aleksander Aas Sjåfjell <aleksander.sjafjell@bekk.no>
2016-05-20 10:31:58 +0200

    Adds xsds for Signature.xml validation

--- Digipost.Signature.Api.Client.Core/Xsd/XsdValidator.cs
+++ Digipost.Signature.Api.Client.Core/Xsd/XsdValidator.cs
@@ -1,5 +1,6 @@
 ﻿using Difi.Felles.Utility;
 using Digipost.Signature.Api.Client.DataTransferObjects.XsdToCode.Code;
+using Digipost.Signature.Api.Client.Resources.Xsd;
 
 namespace Digipost.Signature.Api.Client.Core.Xsd
 {
@@ -7,10 +8,17 @@ namespace Digipost.Signature.Api.Client.Core.Xsd
     {
         public XsdValidator()
         {
-            var httpSigneringPostenNoSchemaV1 = "http://signering.posten.no/schema/v1";
+            const string httpSigneringPostenNoSchemaV1 = "http://signering.posten.no/schema/v1";
+            const string uriEtsi121 = "http://uri.etsi.org/2918/v1.2.1#";
+            const string uriEtsi132 = "http://uri.etsi.org/01903/v1.3.2#";
+            const string xmlDsig = "http://www.w3.org/2000/09/xmldsig#";
+
             AddXsd(httpSigneringPostenNoSchemaV1, XsdFiles.GetCommon());
             AddXsd(httpSigneringPostenNoSchemaV1, XsdFiles.GetDirect());
             AddXsd(httpSigneringPostenNoSchemaV1, XsdFiles.GetPortal());
+            AddXsd(uriEtsi121, XsdResource.Signature.GetTsXsd());
+            AddXsd(uriEtsi132, XsdResource.Signature.GetXadesXsd());
+            AddXsd(xmlDsig, XsdResource.Signature.GetXmldsigCoreXsd());
         }
     }
 }
---
+++ Digipost.Signature.Api.Client.Resources/Xsd/XsdResource.cs
@@ -0,0 +1,37 @@
+﻿using System.IO;
+using System.Text;
+using System.Xml;
+using ApiClientShared;
+using Digipost.Signature.Api.Client.Resources.Xml.Data;
+
+namespace Digipost.Signature.Api.Client.Resources.Xsd
+{
+    internal static class XsdResource
+    {
+        static readonly ResourceUtility ResourceUtility = new ResourceUtility("Digipost.Signature.Api.Client.Resources.Xsd.Data");
+
+        public static class Signature
+        {
+            public static XmlReader GetTsXsd()
+            {
+                return GetResource("ts_102918v010201.xsd");
+            }
+
+            public static XmlReader GetXadesXsd()
+            {
+                return GetResource("XAdES.xsd");
+            }
+
+            public static XmlReader GetXmldsigCoreXsd()
+            {
+                return GetResource("xmldsig-core-schema.xsd");
+            }
+        }
+        private static XmlReader GetResource(params string[] path)
+        {
+            var bytes = ResourceUtility.ReadAllBytes(true, path);
+            return XmlReader.Create(new MemoryStream(bytes));
+        }
+
+    }
+}

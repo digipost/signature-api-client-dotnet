601e8be8b48123b8cc44a9a5967f3299b78504d9
Aleksander Aas Sjåfjell <aleksander.sjafjell@bekk.no>
2016-06-13 10:51:08 +0200

    Adds doc for all enum values of JobStatus

--- Digipost.Signature.Api.Client.Portal/Enums/JobStatus.cs
+++ Digipost.Signature.Api.Client.Portal/Enums/JobStatus.cs
@@ -7,8 +7,19 @@
         /// </summary>
         NoChanges,
 
+        /// <summary>
+        ///     Indicates that the signature job failed. For details about the failure, see the <see cref="SignatureStatus" /> of
+        ///     each signer. When the client confirms a job with this status, the job and its associated resources will become
+        ///     unavailable through the Signature API.
+        /// </summary>
         Failed,
 
+        /// <summary>
+        ///     Indicates that there has been a change to the job, but that it has not been signed by all signers yet. For details
+        ///     about the state, see the <see cref="SignatureStatus" /> of each signer. When the client confirms a job with this
+        ///     status, the job is removed from the queue and will not be returned upon subsequent polling, until the status has
+        ///     changed again.
+        /// </summary>
         InProgress,
 
         /// <summary>

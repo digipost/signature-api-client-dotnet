113ba2084c58e9b3f25b051e3a81617f3d190803
Aleksander Aas Sjåfjell <aleksander.sjafjell@bekk.no>
2016-12-01 11:02:32 +0100

    A bit of cleanup in Logging handler
    Tries to split up in more descriptive methods, long boolean expressions to variables and some other small cleanups. As this is rather hard to test, I have diffed output before and after refactoring, and the output is identical.

--- Digipost.Signature.Api.Client.Core/Internal/LoggingHandler.cs
+++ Digipost.Signature.Api.Client.Core/Internal/LoggingHandler.cs
@@ -35,54 +35,80 @@ namespace Digipost.Signature.Api.Client.Core.Internal
             Log.Debug($"Outgoing: {requestData}");
         }
 
-        private static async Task<string> GetRequestData(HttpRequestMessage request)
-        {
-            var keyValuePairs = new List<KeyValuePair<string, string>>
-            {
-                new KeyValuePair<string, string>("Method", $"{request.Method}, {request.RequestUri}")
-            };
-
-            keyValuePairs.AddRange(SerializeHeaders(request.Headers));
-
-            if ((request.Content != null) && request.Content.IsMimeMultipartContent())
-            {
-                var multipart = await request.Content.ReadAsMultipartAsync().ConfigureAwait(false);
-
-                foreach (var httpContent in multipart.Contents)
-                {
-                    if (httpContent.Headers.ContentType.MediaType == MediaType.ApplicationXml)
-                    {
-                        keyValuePairs.AddRange(SerializeHeaders(request.Content.Headers));
-                        keyValuePairs.Add(new KeyValuePair<string, string>(null, $"{await GetContentData(httpContent).ConfigureAwait(false)}"));
-                    }
-                }
-            }
-
-            return FormatHttpData(keyValuePairs);
-        }
-
         private static async Task LogResponse(HttpResponseMessage response)
         {
             var responseData = await GetResponseData(response).ConfigureAwait(false);
             Log.Debug($"Incoming: {responseData}");
         }
 
+        private static async Task<string> GetRequestData(HttpRequestMessage request)
+        {
+            var requestDescriptionsAndData = new List<KeyValuePair<string, string>>();
+            requestDescriptionsAndData.Add(GetRequestMethodDescription(request));
+            requestDescriptionsAndData.AddRange(GetHeadersDescription(request.Headers));
+            requestDescriptionsAndData.AddRange(await GetRequestContentHeadersDescriptionAndData(request));
+
+            return FormatHttpData(requestDescriptionsAndData);
+        }
+
         private static async Task<string> GetResponseData(HttpResponseMessage response)
         {
-            var keyValuePairs = new List<KeyValuePair<string, string>>
-            {
-                new KeyValuePair<string, string>("StatusCode", $"{(int) response.StatusCode}, {response.StatusCode}")
-            };
-
-            keyValuePairs.AddRange(SerializeHeaders(response.Headers));
-            keyValuePairs.AddRange(SerializeHeaders(response.Content.Headers));
-
-            if ((response.Content != null) && (response.Content.Headers.ContentType?.MediaType == MediaType.ApplicationXml))
-            {
-                keyValuePairs.Add(new KeyValuePair<string, string>(null, $"{await GetContentData(response.Content).ConfigureAwait(false)}"));
-            }
+            var keyValuePairs = new List<KeyValuePair<string, string>>();
+            keyValuePairs.Add(GetResponseStatusCodeDescription(response));
+            keyValuePairs.AddRange(GetHeadersDescription(response.Headers));
+            keyValuePairs.AddRange(GetResponseContentHeaders(response));
+            keyValuePairs.Add(await GetResponseDataIfAny(response, keyValuePairs));
 
             return FormatHttpData(keyValuePairs);
+        }
+
+        private static KeyValuePair<string, string> GetRequestMethodDescription(HttpRequestMessage request)
+        {
+            return new KeyValuePair<string, string>("Method", $"{request.Method}, {request.RequestUri}");
+        }
+
+        private static async Task<IEnumerable<KeyValuePair<string, string>>> GetRequestContentHeadersDescriptionAndData(HttpRequestMessage request)
+        {
+            var contentDescriptionAndData = new List<KeyValuePair<string, string>>();
+            var hasContent = (request.Content != null) && request.Content.IsMimeMultipartContent();
+
+            if (!hasContent) { return contentDescriptionAndData; }
+
+            var multipart = await request.Content.ReadAsMultipartAsync().ConfigureAwait(false);
+
+            foreach (var httpContent in multipart.Contents)
+            {
+                if (httpContent.Headers.ContentType.MediaType != MediaType.ApplicationXml) { continue; }
+
+                contentDescriptionAndData.AddRange(SerializeHeaders(request.Content.Headers));
+                contentDescriptionAndData.Add(new KeyValuePair<string, string>(null, $"{await GetContentData(httpContent).ConfigureAwait(false)}"));
+            }
+
+            return contentDescriptionAndData;
+        }
+
+        private static async Task<KeyValuePair<string, string>> GetResponseDataIfAny(HttpResponseMessage response, List<KeyValuePair<string, string>> keyValuePairs)
+        {
+            var hasContent = (response.Content != null) && (response.Content.Headers.ContentType?.MediaType == MediaType.ApplicationXml);
+
+            if (!hasContent) return new KeyValuePair<string, string>();
+
+            return new KeyValuePair<string, string>(null, $"{await GetContentData(response.Content).ConfigureAwait(false)}");
+        }
+
+        private static KeyValuePair<string, string> GetResponseStatusCodeDescription(HttpResponseMessage response)
+        {
+            return new KeyValuePair<string, string>("StatusCode", $"{(int)response.StatusCode}, {response.StatusCode}");
+        }
+
+        private static List<KeyValuePair<string, string>> GetHeadersDescription(HttpHeaders headers)
+        {
+            return SerializeHeaders(headers);
+        }
+
+        private static IEnumerable<KeyValuePair<string, string>> GetResponseContentHeaders(HttpResponseMessage response)
+        {
+            return SerializeHeaders(response.Content.Headers);
         }
 
         private static List<KeyValuePair<string, string>> SerializeHeaders(HttpHeaders headers)
@@ -110,7 +136,7 @@ namespace Digipost.Signature.Api.Client.Core.Internal
 
         private static Task<string> GetContentData(HttpContent httpContent)
         {
-            return httpContent != null ? httpContent.ReadAsStringAsync() : Task.FromResult(string.Empty);
+            return httpContent?.ReadAsStringAsync() ?? Task.FromResult(string.Empty);
         }
     }
 }

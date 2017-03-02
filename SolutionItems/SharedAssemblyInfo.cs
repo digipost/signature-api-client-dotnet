71afda7828aab7afffc79ba60e783c52d4650115
Aleksander Aas Sjåfjell <aleksander.sjafjell@bekk.no>
2017-03-02 10:17:16 +0100

    Oppgraderer versjonsnummer til 3.0

--- SolutionItems/SharedAssemblyInfo.cs
+++ SolutionItems/SharedAssemblyInfo.cs
@@ -1,11 +1,11 @@
-using System.Reflection;
+﻿using System.Reflection;
 
 [assembly: AssemblyCompany("Posten Norge AS")]
 [assembly: AssemblyTrademark("Posten Norge AS")]
 [assembly: AssemblyProduct("Posten Signature API Client")]
 [assembly: AssemblyDescription("Posten Signature Client library for API integration.")]
-[assembly: AssemblyVersion("2.2.0")]
-[assembly: AssemblyFileVersion("2.2.0")]
-[assembly: AssemblyInformationalVersion("2.2.0")]
+[assembly: AssemblyVersion("3.0.0")]
+[assembly: AssemblyFileVersion("3.0.0")]
+[assembly: AssemblyInformationalVersion("3.0.0")]
 [assembly: AssemblyCulture("")]
 [assembly: AssemblyCopyright("© 2015-2017 Posten Norge AS")]

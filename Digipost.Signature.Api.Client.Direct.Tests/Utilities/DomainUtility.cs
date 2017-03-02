6dbb19ba4250a625d945b189951e4e0e251edcd3
Aleksander Aas Sjåfjell <aleksander.sjafjell@bekk.no>
2017-03-02 10:06:09 +0100

    Generaliserer smoktester til å bruke Identifier i stedet for PIN

--- Digipost.Signature.Api.Client.Direct.Tests/Utilities/DomainUtility.cs
+++ Digipost.Signature.Api.Client.Direct.Tests/Utilities/DomainUtility.cs
@@ -16,9 +16,9 @@ namespace Digipost.Signature.Api.Client.Direct.Tests.Utilities
             return new Job(GetDirectDocument(), GetSigner(), "Reference", GetExitUrls());
         }
 
-        public static Job GetDirectJob(params PersonalIdentificationNumber[] signers)
+        public static Job GetDirectJob(params Identifier[] signers)
         {
-            return new Job(GetDirectDocument(), signers.Select(pin => new Signer(pin)), "Reference", GetExitUrls());
+            return new Job(GetDirectDocument(), signers.Select(pin => new Signer(pin) {SignatureType = SignatureType.AdvancedSignature }), "Reference", GetExitUrls());
         }
 
         public static Job GetDirectJobWithSender()
@@ -26,7 +26,7 @@ namespace Digipost.Signature.Api.Client.Direct.Tests.Utilities
             return new Job(GetDirectDocument(), GetSigner(), "Reference", GetExitUrls(), CoreDomainUtility.GetSender());
         }
 
-        public static Job GetPollableDirectJob(params PersonalIdentificationNumber[] signers)
+        public static Job GetPollableDirectJob(params Identifier[] signers)
         {
             return new Job(GetDirectDocument(), signers.Select(pin => new Signer(pin)), "Reference", GetExitUrls(), statusRetrievalMethod: StatusRetrievalMethod.Polling);
         }

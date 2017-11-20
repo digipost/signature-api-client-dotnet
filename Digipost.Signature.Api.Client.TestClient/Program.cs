a5f8bb1605e700618dd4bb658bea908fa04f61fe
Aleksander Sjåfjell <aleksander.sjafjell@bekk.no>
2017-11-20 14:28:55 +0100

    Enkel smoketest for testklient
    Det hender seg at vi ønsker å teste at alle binding redirects og imports
    for biblioteket fungerer som de skal. Da er det veldig kjekt å ha et
    prosjekt som bruker nuget-pakken og ikke klasser i projsektet. Dette
    gjør vi nå. Det er kanskje litt uheldig at dette ikke vil fungere rett
    ut av boksen for alle, men da kan det testes med minimalt med effort.
    #MVP

--- Digipost.Signature.Api.Client.TestClient/Program.cs
+++ Digipost.Signature.Api.Client.TestClient/Program.cs
@@ -1,7 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Reflection;
 using Common.Logging;
 using Digipost.Signature.Api.Client.Core;
+using Digipost.Signature.Api.Client.Core.Enums;
+using Digipost.Signature.Api.Client.Core.Identifier;
 using Digipost.Signature.Api.Client.Portal;
 using Environment = Digipost.Signature.Api.Client.Core.Environment;
 
@@ -19,10 +22,31 @@ namespace Digipost.Signature.Api.Client.TestClient
             Log.Warn("Warn logging");
             Log.Error("Error logging");
             Log.Fatal("Fatal logging");
-            var client = new PortalClient(new ClientConfiguration(Environment.DifiQa, "‎2d 7f 30 dd 05 d3 b7 fc 7a e5 97 3a 73 f8 49 08 3b 20 40 ed", new Sender("123456789")));
+
+            var stringPrivateOrganizationNumber = "088015814";
+            var stringPublicOrganizationNumber = "988015814";
+
+            var testEnvironment = Environment.DifiTest;
+            testEnvironment.Url = new Uri("https://api.test.signering.posten.no");
+
+            var client = new PortalClient(
+                new ClientConfiguration(testEnvironment, "‎2d 7f 30 dd 05 d3 b7 fc 7a e5 97 3a 73 f8 49 08 3b 20 40 ed", 
+                new Sender(stringPublicOrganizationNumber))
+            );
+
+            var signer = new Signer(new PersonalIdentificationNumber("01043100358"), new Notifications(new Email("email@example.com"))) { OnBehalfOf = OnBehalfOf.Other };
+
+            var created = client.Create(new Job(
+                new Document("Et signeringsoppdrag", "Her kommer en melding", FileType.Pdf, @"\\vmware-host\Shared Folders\Downloads\00370726201232902222.pdf"),
+                new List<Signer>() {signer},
+                "Avsenders referance", new Sender(stringPrivateOrganizationNumber))).Result;
+
+            var status = client.GetStatusChange(new Sender(stringPrivateOrganizationNumber)).Result;
+            
+
 
             Console.WriteLine("Finished with loggah ...");
-            Console.ReadLine();
+            //Console.ReadLine();
         }
     }
 }

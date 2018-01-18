01380986e864510beb334dafc9effcda0f5d9adc
Aleksander Sjåfjell <aleksander.sjafjell@bekk.no>
2018-01-18 10:20:16 +0100

    Går over til å bruke RSA i stedet for RSACryptoServiceProvider
    Fordi sistnevnte kun er støttet i Windows-land. Ingne garanti for at
    dette fungerer plettfritt på .NET eller i de andre testene. Men denne
    fører i alle fall til at 5 ekstra tester kjører

--- Digipost.Signature.Api.Client.Core/Internal/Asice/AsiceSignature/SignedXmlWithAgnosticId.cs
+++ Digipost.Signature.Api.Client.Core/Internal/Asice/AsiceSignature/SignedXmlWithAgnosticId.cs
@@ -57,49 +57,41 @@ namespace Digipost.Signature.Api.Client.Core.Internal.Asice.AsiceSignature
         private static void AddSignatureMethodToCryptoApi(string signatureMethod)
         {
             if (CryptoConfig.CreateFromName(signatureMethod) == null)
-            {
                 CryptoConfig.AddAlgorithm(typeof(RsaPkCs1Sha256SignatureDescription), signatureMethod);
-            }
         }
 
         private static void AssertHasPrivateKeyOrThrow(X509Certificate2 certificate)
         {
             if (!certificate.HasPrivateKey)
-            {
                 throw new SecurityException($"Specified certificate with fingerprint {certificate.Thumbprint} does not contain a private key, which is mandatory when signing XML documents.");
-            }
         }
 
         private void SetSigningKey(X509Certificate2 certificate)
         {
             var targetKey = ExtractValidPrivateKeyOrThrow(certificate);
 
-            if (targetKey.CspKeyContainerInfo.ProviderType == RsaSha256DigitalSignaturesCryptoApiProvider)
-            {
+//            if (targetKey.CspKeyContainerInfo.ProviderType == RsaSha256DigitalSignaturesCryptoApiProvider)
                 SigningKey = targetKey;
-            }
-            else
-            {
-                SigningKey = new RSACryptoServiceProvider();
-                try
-                {
-                    SigningKey.FromXmlString(certificate.PrivateKey.ToXmlString(true));
-                }
-                catch (Exception e)
-                {
-                    throw new Exception($"Specified certificate with fingerprint {certificate.Thumbprint} cannot be exported. This is required when the certificate isn't created with 'Microsoft Enhanced RSA and AES Cryptographic Provider' as CryptoAPI provider name (-sp parameter i makecert.exe or -csp parameter i openssl).", e);
-                }
-            }
+//            else
+//            {
+//                SigningKey = new RSACryptoServiceProvider();
+//                try
+//                {
+//                    SigningKey.FromXmlString(certificate.PrivateKey.ToXmlString(true));
+//                }
+//                catch (Exception e)
+//                {
+//                    throw new Exception($"Specified certificate with fingerprint {certificate.Thumbprint} cannot be exported. This is required when the certificate isn't created with 'Microsoft Enhanced RSA and AES Cryptographic Provider' as CryptoAPI provider name (-sp parameter i makecert.exe or -csp parameter i openssl).", e);
+//                }
+            //}
         }
 
-        private static RSACryptoServiceProvider ExtractValidPrivateKeyOrThrow(X509Certificate2 certificate)
+        private static RSA ExtractValidPrivateKeyOrThrow(X509Certificate2 certificate)
         {
-            var targetKey = certificate.PrivateKey as RSACryptoServiceProvider;
+            var targetKey = certificate.GetRSAPrivateKey();
             if (targetKey == null)
 
-            {
                 throw new SecurityException($"Specified certificate with fingerprint {certificate.Thumbprint} is not a valid RSA asymetric key.");
-            }
 
             return targetKey;
         }
@@ -109,9 +101,7 @@ namespace Digipost.Signature.Api.Client.Core.Internal.Asice.AsiceSignature
             SignedInfo.SignatureMethod = SignatureMethod;
             SignedInfo.CanonicalizationMethod = CanocalizationMethod;
             if (inclusiveNamespacesPrefixList != null)
-            {
                 ((XmlDsigExcC14NTransform) SignedInfo.CanonicalizationMethodObject).InclusiveNamespacesPrefixList = inclusiveNamespacesPrefixList;
-            }
         }
 
         public override XmlElement GetIdElement(XmlDocument doc, string id)
@@ -155,9 +145,7 @@ namespace Digipost.Signature.Api.Client.Core.Internal.Asice.AsiceSignature
                 result = node.SelectSingleNode($"//*[@*[local-name() = '{s}'] = '{idValue}']") as XmlElement;
 
                 if (result != null)
-                {
                     break;
-                }
             }
 
             return result;

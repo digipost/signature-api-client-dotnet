99eee93f6f4f192053772d8e9041d208019c645c
Aleksander Sjåfjell <aleksander.sjafjell@bekk.no>
2018-10-10 15:49:55 +0200

    Utkommenterer kode i Program fram til vi har bygd første 5.0-pakkeˆ

--- Digipost.Signature.Api.Client.Program/Program.cs
+++ Digipost.Signature.Api.Client.Program/Program.cs
@@ -1,14 +1,7 @@
 ﻿using System;
-using System.IO;
-using System.Text.RegularExpressions;
-using Digipost.Signature.Api.Client.Core;
-using Digipost.Signature.Api.Client.Direct;
-using Digipost.Signature.Api.Client.Portal;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using NLog.Extensions.Logging;
-using Environment = Digipost.Signature.Api.Client.Core.Environment;
 
 namespace Digipost.Signature.Api.Client.Program
 {
@@ -16,16 +9,16 @@ namespace Digipost.Signature.Api.Client.Program
     {
         static void Main(string[] args)
         {
-            ClientConfiguration clientConfiguration = new ClientConfiguration(Environment.Test,  CertificateReader.ReadCertificate());
+//            ClientConfiguration clientConfiguration = new ClientConfiguration(Environment.Test,  CertificateReader.ReadCertificate());
 //            PortalClient portalClient = new PortalClient(clientConfiguration);
-            var serviceProvider = CreateServiceProvider(clientConfiguration);
+//            var serviceProvider = CreateServiceProvider(clientConfiguration);
 
-            SetUpNLog(serviceProvider);
+//            SetUpNLog(serviceProvider);
             
 //            new DirectClient()
 
-            var portalClient = serviceProvider.GetService<PortalClient>();
-            portalClient.GetRootResource(new Sender("988015814"));
+//            var portalClient = serviceProvider.GetService<PortalClient>();
+//            portalClient.GetRootResource(new Sender("988015814"));
         }
 
         private static void SetUpNLog(IServiceProvider serviceProvider)
@@ -36,17 +29,17 @@ namespace Digipost.Signature.Api.Client.Program
             NLog.LogManager.LoadConfiguration("/Users/aas/projects/digipost/signature-api-client-dotnet/Digipost.Signature.Api.Client.Program/nlog.config");
         }
 
-        private static IServiceProvider CreateServiceProvider(ClientConfiguration clientConfiguration)
-        {
-            var services = new ServiceCollection();
-            
-            services.AddSingleton<ILoggerFactory, LoggerFactory>();
-            services.AddSingleton(typeof(ILogger<>), typeof(Logger<>));
-            services.AddLogging((builder) => builder.SetMinimumLevel(LogLevel.Trace));
-            services.AddSingleton(clientConfiguration);
-            services.AddSingleton<PortalClient>();
-
-            return services.BuildServiceProvider();
-        }
+//        private static IServiceProvider CreateServiceProvider(ClientConfiguration clientConfiguration)
+//        {
+//            var services = new ServiceCollection();
+//            
+//            services.AddSingleton<ILoggerFactory, LoggerFactory>();
+//            services.AddSingleton(typeof(ILogger<>), typeof(Logger<>));
+//            services.AddLogging((builder) => builder.SetMinimumLevel(LogLevel.Trace));
+//            services.AddSingleton(clientConfiguration);
+//            services.AddSingleton<PortalClient>();
+//
+//            return services.BuildServiceProvider();
+//        }
     }
 }

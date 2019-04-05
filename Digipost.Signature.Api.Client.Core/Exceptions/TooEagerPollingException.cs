478dda157476373f829b7483e9f039e8ceaa1522
Aleksander Sjåfjell <aleksander.sjafjell@bekk.no>
2019-04-05 12:53:40 +0200

    Legger til støtte for DateTime som input til TooEagerPollingException
    Fordi det er fint at klassen selv kan håndtere de to formatene mtp.
    lesbarhet i kode ellers.

--- Digipost.Signature.Api.Client.Core/Exceptions/TooEagerPollingException.cs
+++ Digipost.Signature.Api.Client.Core/Exceptions/TooEagerPollingException.cs
@@ -4,13 +4,19 @@ namespace Digipost.Signature.Api.Client.Core.Exceptions
 {
     public class TooEagerPollingException : SignatureException
     {
-        public TooEagerPollingException(string nextPermittedPollTime)
-            : base($"Excessive polling is not allowed. The next permitted poll time is: {nextPermittedPollTime}, and can also be found in variable 'NextPermittedPollTime' on exception class.")
-        {
-            NextPermittedPollTime = DateTime.Parse(nextPermittedPollTime).ToUniversalTime();
-        }
 
-        public DateTime NextPermittedPollTime { get; set; }
+        public TooEagerPollingException(string nextPermittedPollTime)
+            : this(DateTime.Parse(nextPermittedPollTime))
+        {
+        }
+        
+        public TooEagerPollingException(DateTime nextPermittedPollTime)
+            : base($"Excessive polling is not allowed. The next permitted poll time is: {nextPermittedPollTime.ToUniversalTime().ToString("O")}, and can also be found in variable 'NextPermittedPollTime' on exception class.")
+        {
+            NextPermittedPollTime = nextPermittedPollTime;
+        }
+        
+        public DateTime NextPermittedPollTime { get; }
 
         public override string ToString()
         {

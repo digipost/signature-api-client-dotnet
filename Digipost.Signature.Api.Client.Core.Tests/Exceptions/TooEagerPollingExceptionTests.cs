9d3b0c1b6b8b9f3ce730a29e3f7dab3d022e4f93
Aleksander Sjåfjell <aleksander.sjafjell@bekk.no>
2019-04-05 12:53:40 +0200

    Legger til støtte for DateTime som input til TooEagerPollingException
    Fordi det er fint at klassen selv kan håndtere de to formatene mtp.
    lesbarhet i kode ellers.

--- Digipost.Signature.Api.Client.Core.Tests/Exceptions/TooEagerPollingExceptionTests.cs
+++ Digipost.Signature.Api.Client.Core.Tests/Exceptions/TooEagerPollingExceptionTests.cs
@@ -8,19 +8,38 @@ namespace Digipost.Signature.Api.Client.Core.Tests.Exceptions
     {
         public class ConstructorMethod : TooEagerPollingExceptionTests
         {
+            private const string ExpectedInToString = "2019-04-22T09:39:46.6100000Z";
+            private static readonly DateTime ExpectedDateTime = new DateTime(2019, 04, 22, 09, 39, 46, 610, DateTimeKind.Utc);
+
             [Fact]
             public void Initialize_with_date_string()
             {
                 //Arrange
-                const string source = "2016-11-10T10:39:46.610+01:00";
-                var expected = new DateTime(2016, 11, 10, 09, 39, 46, 610, DateTimeKind.Utc);
+                const string source = "2019-04-22T10:39:46.610+01:00";
+                
+                //Act
+                var tooEagerPollingException = new TooEagerPollingException(source);
+
+                //Assert
+                Assert.Equal(ExpectedDateTime, tooEagerPollingException.NextPermittedPollTime.ToUniversalTime());
+                Assert.Contains(ExpectedInToString, tooEagerPollingException.Message);
+            }
+            
+            [Fact]
+            public void Initialize_with_date_time()
+            {
+                //Arrange
+                var source = new DateTime(2019, 04, 22, 09, 39, 46, 610, DateTimeKind.Utc);
+                
 
                 //Act
                 var tooEagerPollingException = new TooEagerPollingException(source);
 
                 //Assert
-                Assert.Equal(expected, tooEagerPollingException.NextPermittedPollTime.ToUniversalTime());
+                Assert.Equal(ExpectedDateTime, tooEagerPollingException.NextPermittedPollTime.ToUniversalTime());
+                Assert.Contains(ExpectedInToString, tooEagerPollingException.Message);
             }
+
         }
     }
 }

8c211152d4e6dbf78d4f7784f5d0f6a546f6061f
Aleksander Sjåfjell <aleksander.sjafjell@bekk.no>
2019-04-05 10:23:48 +0200

    Ignorerer sekundforskjeller i test-sammenligning
    I noen tilfeller så blir det krøll i sammenligning av tid på grunn
    av avrunding eller noe slikt. Denne fiksen gjør at vi ikke hensyntar
    forskjeller på opp til 999 Ms

--- Digipost.Signature.Api.Client.Core.Tests/Utilities/CompareObjects/Comparator.cs
+++ Digipost.Signature.Api.Client.Core.Tests/Utilities/CompareObjects/Comparator.cs
@@ -9,6 +9,7 @@ namespace Digipost.Signature.Api.Client.Core.Tests.Utilities.CompareObjects
         public bool AreEqual(object expected, object actual)
         {
             var compareLogic = new CompareLogic();
+            compareLogic.Config.MaxMillisecondsDateDifference = 999;
             return compareLogic.Compare(expected, actual).AreEqual;
         }
 

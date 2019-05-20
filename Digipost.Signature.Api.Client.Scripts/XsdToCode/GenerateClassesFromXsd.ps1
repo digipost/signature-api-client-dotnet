9ff628ccf2e6aaad41718a4fe1374454d0bd3de3
Aleksander Sjåfjell <aleksander.sjafjell@bekk.no>
2019-05-20 13:43:10 +0200

    Oppdaterer til ny versjon av XSD
    - Oppdaterer scriptet til å kunne kjøres fra Mac
    - Legger inn ny versjon av direct.xsd
    - Oppdaterer genererte klasser

--- Digipost.Signature.Api.Client.Scripts/XsdToCode/GenerateClassesFromXsd.ps1
+++ Digipost.Signature.Api.Client.Scripts/XsdToCode/GenerateClassesFromXsd.ps1
@@ -1,18 +1,18 @@
-# RUNNING THE FIRST TIME
-#   Set-ExecutionPolicy Bypass
+# Running for the first time:
+# Install Powershell with command ...
+# brew cask install powershell
 
-Write-Host "- If on Vm-Ware host and script on Z: drive:"
-Write-Host "   net use Z: `"\\vmware-host\Shared Folders`""
 
-function GenerateCode($XsdPath, $OutDir)
+function GenerateCode($XsdDirectory, $RootXsd, $OutDir)
 {
-    Write-Host("XSD: $Xsd")
-    Write-Host("OUT: $OutDir")
-    
-    $CurrentDirectory = (Get-Item -Path ".\" -Verbose).FullName
-    Set-Location "C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.6.1 Tools"    
-    Invoke-Expression ".\Xsd.exe $XsdPath /classes /out:$OutDir"
-    Set-Location $CurrentDirectory
+    Write-Host("Starting to generate code from XSD ...")
+    Write-Host("Xsd-directory: $XsdDirectory")
+    Write-Host("Output-directory: $OutDir")
+
+    Write-Host("Changing directory to $XsdDirectory ...")
+    Set-Location $XsdDirectory
+
+    Invoke-Expression "xsd $RootXsd /classes /outputdir:$OutDir"
 }
 
 function Get-ScriptDirectory
@@ -22,11 +22,10 @@ function Get-ScriptDirectory
 }
 
 $CurrentScriptDir = Get-ScriptDirectory
-
-$Xsd = "\Xsd\direct-and-portal.xsd"
-$XsdPath = "$CurrentScriptDir$Xsd"
-
 $XsdToCodeDir = (Get-Item $CurrentScriptDir)
-$CodeDir =  "$XsdToCodeDir\Code"
 
-GenerateCode $XsdPath "$CodeDir"
+$CodeDir = "$XsdToCodeDir/Code"
+$XsdPath = "$CurrentScriptDir/Xsd/"
+$RootXsd = "direct-and-portal.xsd"
+
+GenerateCode "$XsdPath" "$RootXsd" "$CodeDir"

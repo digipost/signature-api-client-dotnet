420977050c8d6cd87e7eaed80d79704087ef8617
Simen Støa <simen.stoa@bekk.no>
2020-02-20 10:16:37 +0100

    Resets csproj for Program

--- Digipost.Signature.Api.Client.Program/Program.cs
+++ Digipost.Signature.Api.Client.Program/Program.cs
@@ -1,14 +1,20 @@
 ﻿
+using System;
+using Digipost.Signature.Api.Client.Core;
+using Digipost.Signature.Api.Client.Portal;
+using Environment = Digipost.Signature.Api.Client.Core.Environment;
+
 namespace Digipost.Signature.Api.Client.Program
 {
     public class Program
     {
         static void Main(string[] args)
         {
-//            var clientConfiguration = new ClientConfiguration(Environment.Test, certificateReader2.ReadCertificate());
-//            var portalClient = new PortalClient(clientConfiguration);
+            /*var clientConfiguration = new ClientConfiguration(Environment.DifiQa, CertificateReader.ReadCertificate());
+            var portalClient = new PortalClient(clientConfiguration);
 
-//            var result = portalClient.GetRootResource(new Sender("988015814")).Result;
+            var result = portalClient.GetRootResource(new Sender("988015814")).Result;
+            Console.WriteLine(result);*/
         }
     }
 }

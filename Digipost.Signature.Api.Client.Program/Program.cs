4bea6cbd8a158e746cf08793cb0ee476015c90d8
Simen Støa <simen.stoa@bekk.no>
2020-02-20 11:42:01 +0100

    Removed unused imports from Program

--- Digipost.Signature.Api.Client.Program/Program.cs
+++ Digipost.Signature.Api.Client.Program/Program.cs
@@ -1,9 +1,4 @@
 ﻿
-using System;
-using Digipost.Signature.Api.Client.Core;
-using Digipost.Signature.Api.Client.Portal;
-using Environment = Digipost.Signature.Api.Client.Core.Environment;
-
 namespace Digipost.Signature.Api.Client.Program
 {
     public class Program
